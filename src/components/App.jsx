8cc101b00ebdcd5f8f647956458e400e13492e36
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-05-14 10:55:35 +0300

    start commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React marvel app template
+      React marvel app template from team 6
     </div>
   );
 };
